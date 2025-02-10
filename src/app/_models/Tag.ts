fea294e5a25c197b4e7e3102fbf1f62dd33e707e
z4rf <raminzarf@gmail.com>
2025-02-09 22:12:14 -0500

    diversified tag colors

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -1,14 +1,14 @@
 export class Tag {
     static readonly ANGULAR = new Tag('Angular', 'red');
-    static readonly PYTHON = new Tag('Python', 'darkred');
-    static readonly DATA_STRUCTURE = new Tag('Data Structure', 'darkred');
-    static readonly IOS = new Tag('iOS', 'darkred');
-    static readonly NETWORKING = new Tag('Networking', 'darkred');
-    static readonly SWIFT = new Tag('Swift', 'darkred');
-    static readonly REACT = new Tag('React', 'darkred');
-    static readonly NODE = new Tag('Node', 'darkred');
-    static readonly C = new Tag('C', 'darkred');
-    static readonly ART = new Tag('Art', 'darkred');
+    static readonly PYTHON = new Tag('Python', 'darkgreen');
+    static readonly DATA_STRUCTURE = new Tag('Data Structure', 'darkblue');
+    static readonly IOS = new Tag('iOS', 'purple');
+    static readonly NETWORKING = new Tag('Networking', 'blue');
+    static readonly SWIFT = new Tag('Swift', 'orange');
+    static readonly REACT = new Tag('React', 'lightblue');
+    static readonly NODE = new Tag('Node', 'pink');
+    static readonly C = new Tag('C', 'brown');
+    static readonly ART = new Tag('Art', 'grey');
 
 
     private constructor(private readonly key: string, public readonly color: string) {
